950931546534271d19dea3e549495f0c6e2bbfe2
geekyJock8 <vikas.rajput54@gmail.com>
2018-02-10 02:01:34 +0530

    Added Navigation System(Not Working)

---
+++ Assets/Scripts/movementScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class movementScript : MonoBehaviour {
+
+    private GameObject player;
+
+    private NavMeshAgent navMeshAgent;
+
+    private NavMeshHit navMeshHit;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        navMeshAgent = player.GetComponent<NavMeshAgent>();
+	}
+	
+	public void OnPointerDown()
+    {
+        print("OnPointerDown Called!");
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100f))
+        {
+            Debug.DrawLine(navMeshAgent.gameObject.transform.position, hit.point, Color.red, 10f);
+            navMeshAgent.SetDestination(hit.point);
+        }
+    }
+}
