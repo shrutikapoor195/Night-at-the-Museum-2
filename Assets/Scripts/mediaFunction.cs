33060af44349abb349653ae0744046bb437b9c3d
geekyJock8 <vikas.rajput54@gmail.com>
2018-02-12 02:01:10 +0530

    Fixed Audio Problem

--- Assets/Scripts/mediaFunction.cs
+++ Assets/Scripts/mediaFunction.cs
@@ -7,26 +7,17 @@ using UnityEngine.Video;
 
 public class mediaFunction : MonoBehaviour {
 
-    public VideoClip videoToPlay;
     private AudioSource audioSource;
     public GameObject player;
 
-    private VideoPlayer videoPlayer;
+    public VideoPlayer videoPlayer;
     private VideoSource videoSource;
 
 	// Use this for initialization
 	void Start () {
-        videoPlayer = gameObject.AddComponent<VideoPlayer>();
-        audioSource = gameObject.AddComponent<AudioSource>();
 
         Application.runInBackground = true;
         videoPlayer.source = VideoSource.VideoClip;
-
-        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
-        videoPlayer.EnableAudioTrack(0, true);
-        videoPlayer.SetTargetAudioSource(0, audioSource);
-
-        videoPlayer.clip = videoToPlay;
 
         videoPlayer.Prepare();
     }
@@ -34,17 +25,13 @@ public class mediaFunction : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float distance = Vector3.Distance(this.transform.position, player.transform.position);
-        if(distance < 3.0f && videoPlayer.isPrepared)
+        if(distance < 4.0f && videoPlayer.isPrepared)
         {
             videoPlayer.Play();
-            audioSource.Play();
-            print(audioSource.name);
-            print(audioSource.clip.name);
         }
         if(distance > 3.0f)
         {
             videoPlayer.Pause();
-            audioSource.Pause();
         }
 	}
 }
