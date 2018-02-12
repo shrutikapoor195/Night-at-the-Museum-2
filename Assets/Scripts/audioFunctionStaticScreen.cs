5df21497f88be40be9f5c3ba8fdf017e46ba90ca
geekyJock8 <vikas.rajput54@gmail.com>
2018-02-13 01:02:19 +0530

    Added 2 new Audio Sources to the scene

---
+++ Assets/Scripts/audioFunctionStaticScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class audioFunctionStaticScreen : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public GameObject player;
+    public float activationRadius;
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(this.transform.position, player.transform.position);
+        if (distance < activationRadius && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+        if (distance > activationRadius && audioSource.isPlaying)
+        {
+            audioSource.Pause();
+        }
+    }
+}
--- Assets/Scripts/mediaFunction.cs
+++ Assets/Scripts/mediaFunction.cs
@@ -25,11 +25,11 @@ public class mediaFunction : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float distance = Vector3.Distance(this.transform.position, player.transform.position);
-        if(distance < 4.0f && videoPlayer.isPrepared)
+        if(distance < 4.0f && videoPlayer.isPrepared && !videoPlayer.isPlaying)
         {
             videoPlayer.Play();
         }
-        if(distance > 3.0f)
+        if(distance > 4.0f && videoPlayer.isPlaying)
         {
             videoPlayer.Pause();
         }
